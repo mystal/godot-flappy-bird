4b16f6c6547f9bf4803a9ebf6576ede3ba444b31
Gabriel Martinez <reitaka@gmail.com>
2023-07-14 17:03:14 -0400

    Use tabs in globals.gd and update editorconfig to use tab indent in GDscript

--- scenes/globals.gd
+++ scenes/globals.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 enum GameState {
-    Ready,
-    Playing,
-    Lost,
+	Ready,
+	Playing,
+	Lost,
 }
 
 # TODO: Can we compute these instead of hard-coding?
@@ -17,34 +17,34 @@ var score: int = 0
 var last_pipe_y: float = GAME_SIZE.y / 2.0
 
 func gen_random_pipe_y() -> float:
-    # Top of screen plus some buffer room.
-    const MIN: float = PIPE_GAP * 0.75
-    # Top of ground minus some buffer room.
-    const MAX: float = GROUND_Y - (PIPE_GAP * 0.75)
+	# Top of screen plus some buffer room.
+	const MIN: float = PIPE_GAP * 0.75
+	# Top of ground minus some buffer room.
+	const MAX: float = GROUND_Y - (PIPE_GAP * 0.75)
 
-    var range_min: float = max(last_pipe_y - PIPE_Y_RAND_RANGE, MIN)
-    var range_max: float = min(last_pipe_y + PIPE_Y_RAND_RANGE, MAX)
+	var range_min: float = max(last_pipe_y - PIPE_Y_RAND_RANGE, MIN)
+	var range_max: float = min(last_pipe_y + PIPE_Y_RAND_RANGE, MAX)
 
-    last_pipe_y = roundf(randf_range(range_min, range_max))
-    return last_pipe_y
+	last_pipe_y = roundf(randf_range(range_min, range_max))
+	return last_pipe_y
 
 func is_ready() -> bool:
-    return game_state == GameState.Ready
+	return game_state == GameState.Ready
 
 func is_playing() -> bool:
-    return game_state == GameState.Playing
+	return game_state == GameState.Playing
 
 func is_lost() -> bool:
-    return game_state == GameState.Lost
+	return game_state == GameState.Lost
 
 func reset():
-    game_state = GameState.Ready
-    score = 0
-    last_pipe_y = GAME_SIZE.y / 2.0
+	game_state = GameState.Ready
+	score = 0
+	last_pipe_y = GAME_SIZE.y / 2.0
 
 func play():
-    game_state = GameState.Playing
+	game_state = GameState.Playing
 
 func lose():
-    game_state = GameState.Lost
-    print("LOSE")
+	game_state = GameState.Lost
+	print("LOSE")
